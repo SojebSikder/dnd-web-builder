87bcffe2a547764e1bf027a98ebf83a5a484897e
sojebsikder <sojebsikder@gmail.com>
2026-03-02 14:07:55 +0600

    feat: add docs

--- src/lib/editor/plugin/types.ts
+++ src/lib/editor/plugin/types.ts
@@ -5,6 +5,9 @@ export type BlockPlugin = {
   renderer: BlockRenderer;
   defaultSettings?: Record<string, any>;
   settingsSchema?: SettingSchema[];
+  // isDynamic: if true, the editor will not re-render this block on settings changes.
+  // Useful for blocks with internal DOM state or interactive elements (like tables, charts, etc.),
+  // where re-rendering would break event listeners or user input.
   isDynamic?: true;
 };
 
